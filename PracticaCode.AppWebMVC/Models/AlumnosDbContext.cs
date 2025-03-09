66e143b0cc3c5954428e2cfa77d72e83b0ac531b
CHATO <colochocristian@outlook.com>
2025-03-09 11:23:18 -0600

    Agregando codigo Al DbContext

---
+++ PracticaCode.AppWebMVC/Models/AlumnosDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace PracticaCode.AppWebMVC.Models
+{
+    public class AlumnosDbContext: DbContext
+    {
+        public AlumnosDbContext(DbContextOptions<AlumnosDbContext> options) : base(options) { }
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
