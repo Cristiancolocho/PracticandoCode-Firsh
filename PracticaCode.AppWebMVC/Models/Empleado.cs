96d3e2708e0e7a6f0d8f360971c67cd3c3f80d01
CHATO <colochocristian@outlook.com>
2025-03-09 10:56:57 -0600

    Agregando codigo a la clase Empleado

--- PracticaCode.AppWebMVC/Models/Empleado.cs
+++ PracticaCode.AppWebMVC/Models/Empleado.cs
@@ -2,5 +2,11 @@
 {
     public class Empleado
     {
+        public int Id  { get; set; }
+        public string? Nombre { get; set; }
+        public string? Telefono { get; set; }
+        public string? DUI { get; set; }
+        public string? Direccion { get; set; }
+        public string? Email { get; set; }
     }
 }
